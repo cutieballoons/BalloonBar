4550641e46724d6a835c40824c24b03a5f4f1cfb
cutieballoons <hello@cutieballoons.com>
2025-05-19 18:23:48 +0000

    Move balloon images to public/balloons

---
+++ .next/static/chunks/app/layout-d043ac84bff0ac04.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1434:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6986,23))},6986:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[67,441,684,358],()=>s(1434)),_N_E=e.O()}]);
